742049c5edac9d2677e8dcebc47d4947fc9e8aab
447428054 <447428054@qq.com>
2018-03-20 22:55:10 +0800

    docx，pdf的提取

--- Upload/models.py
+++ Upload/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class NormalUser(models.Model):
     username=models.CharField('用户名',max_length=30)
     #用户名
-    headImg=models.FileField('文件',upload_to='./upload')#文件名
+    headImg=models.FileField('文件',upload_to=r'F:\github\WBFL\TianWen\media\upload')#文件名
     def __str__(self):
         return self.username
 
--- Upload/views.py
+++ Upload/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render, render_to_response
 from django import  forms
 from django.http import  HttpResponse
 from .models import NormalUser
+import docx
+from pdfminer.pdfparser import PDFParser,PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LTTextBoxHorizontal,LAParams
+from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
+from . import models
+
 
 # Create your views here.
 class NormalUserForm(forms.Form):
@@ -22,7 +30,73 @@ def registerNormalUser(request):
             normalUser.username = username
             normalUser.headImg = headImg
             normalUser.save()#保存到数据库表中
+            suffix = headImg.name[headImg.name.find('.'):len(headImg.name) + 1]
+            path="F:\\github\\WBFL\\TianWen\\media\\upload\\"+headImg.name
+            if suffix=='.docx':
+                read_doc(request,path)
+            elif suffix=='.pdf':
+                read_pdf(request,path)
+            elif suffix=='.ppt':
+                read_doc(request, path)
+
             return HttpResponse('Upload Succeed!')#重定向显示内容（跳转后内容）
     else:
         uf=NormalUserForm()#刚显示时，实例化空表单
     return render(request,'Upload/register.html',{'uf':uf})#只有刚显示时才起作用
+
+def read_doc(request,path):  #阅读docx文档,path为文件的路径
+       store_path = r'F:\github\WBFL\uploadpath\\'  #txt文件保存的路径
+       doc = docx.Document(path)  #根据路径打开一个docx对象
+       file_name = path[path.rfind('\\') + 1:path.find('.')]  #提取路径中的文件名,不包含后缀
+       outfile = open(store_path + file_name + '_temp.txt','w')   #创建一个临时的txt文件
+       for i in range(len(doc.paragraphs)):  #将docx文档里的数据写入txt文档中
+           outfile.write(doc.paragraphs[i].text + '\n')
+       outfile.close()  #结束写入
+
+
+def read_pdf(request, path):
+    store_path = r'F:\github\WBFL\uploadpath\\'  # txt文件保存的路径
+    fp = open(path, 'rb')  # 以二进制读模式打开
+    file_name = path[path.rfind('\\') + 1:path.find('.')]  # 提取路径中的文件名,不包含后缀
+    outfile = open(store_path + file_name + '_temp.txt', 'w')  # 创建一个临时的txt文件
+    # 用文件对象来创建一个pdf文档分析器
+    praser = PDFParser(fp)
+    # 创建一个PDF文档
+    doc = PDFDocument()
+    # 连接分析器 与文档对象
+    praser.set_document(doc)
+    doc.set_parser(praser)
+
+    # 提供初始化密码
+    # 如果没有密码 就创建一个空的字符串
+    doc.initialize()
+
+    # 检测文档是否提供txt转换，不提供就忽略
+    if not doc.is_extractable:
+        raise PDFTextExtractionNotAllowed
+    else:
+        # 创建PDf 资源管理器 来管理共享资源
+        rsrcmgr = PDFResourceManager()
+        # 创建一个PDF设备对象
+        laparams = LAParams()
+        device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+        # 创建一个PDF解释器对象
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+        # 循环遍历列表，每次处理一个page的内容
+        for page in doc.get_pages():  # doc.get_pages() 获取page列表
+            interpreter.process_page(page)
+            # 接受该页面的LTPage对象
+            layout = device.get_result()
+            # 这里layout是一个LTPage对象 里面存放着 这个page解析出的各种对象 一般包括LTTextBox, LTFigure, LTImage, LTTextBoxHorizontal 等等 想要获取文本就获得对象的text属性，
+            for x in layout:
+                if (isinstance(x, LTTextBoxHorizontal)):
+                    results = x.get_text()
+                    print(results)
+                    outfile.write(results + '\n')
+    outfile.close()
+
+
+
+
+
