5ed4e87125f8caf562c73fb2df84e0e053742d4d
447428054 <447428054@qq.com>
2018-03-22 22:17:05 +0800

    PPT

--- Upload/views.py
+++ Upload/views.py
@@ -3,14 +3,15 @@ from django import forms
 from django.http import HttpResponse
 from .models import NormalUser
 import docx
-<<<<<<< HEAD
+import win32com
+from win32com.client import Dispatch, constants
+
 from pdfminer.pdfparser import PDFParser,PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import PDFPageAggregator
 from pdfminer.layout import LTTextBoxHorizontal,LAParams
 from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
-=======
->>>>>>> 9f36014a873b3d7ceed0413346a988b74c49858f
+
 from . import models
 
 
@@ -35,7 +36,7 @@ def registerNormalUser(request):
             normalUser.username = username
             normalUser.headImg = headImg
             normalUser.save()#保存到数据库表中
-<<<<<<< HEAD
+
             suffix = headImg.name[headImg.name.find('.'):len(headImg.name) + 1]
             path="F:\\github\\WBFL\\TianWen\\media\\upload\\"+headImg.name
             if suffix=='.docx':
@@ -43,7 +44,7 @@ def registerNormalUser(request):
             elif suffix=='.pdf':
                 read_pdf(request,path)
             elif suffix=='.ppt':
-                read_doc(request, path)
+                read_ppt(request, path)
 
             return HttpResponse('Upload Succeed!')#重定向显示内容（跳转后内容）
     else:
@@ -102,36 +103,28 @@ def read_pdf(request, path):
                     outfile.write(results + '\n')
     outfile.close()
 
+def read_ppt(request, path):
+    store_path = r'F:\github\WBFL\uploadpath\\'  # txt文件保存的路径
+    # fp = open(path, 'rb')  # 以二进制读模式打开
+    file_name = path[path.rfind('\\') + 1:path.find('.')]  # 提取路径中的文件名,不包含后缀
+
+    ppt = win32com.client.Dispatch('PowerPoint.Application')
+    ppt.Visible = 1
+
+    pptSel = ppt.Presentations.Open(path)
+    win32com.client.gencache.EnsureDispatch('PowerPoint.Application')
+    f = open(store_path + file_name + '_temp.txt', 'w')   # 创建一个临时的txt文件
+    slide_count = pptSel.Slides.Count
+    for i in range(1, slide_count + 1):
+        shape_count = pptSel.Slides(i).Shapes.Count
+        print
+        shape_count
+        for j in range(1, shape_count + 1):
+            if pptSel.Slides(i).Shapes(j).HasTextFrame:
+                s = pptSel.Slides(i).Shapes(j).TextFrame.TextRange.Text
+                f.write(s.encode('utf-8') + "\n")
+    f.close()
+    ppt.Quit()
 
 
 
-
-=======
-            #headImg.name仅仅是文件的名字，不包含路径
-            suffix = headImg.name[headImg.name.find('.'):len(headImg.name) + 1]   #获取文件后缀名
-            path = "G:/" + headImg.name  #文件的路径，使用时按文件保存的位置进行修改
-            if suffix == '.docx':
-                read_doc(request,path)
-            elif suffix == '.pdf':
-                read_doc(headImg,path)
-            elif suffix == '.doc':
-                read_doc(headImg,path)
-            return HttpResponse('Upload Succeed!')  #重定向显示内容（跳转后内容）
-    else:
-        uf=NormalUserForm()  #刚显示时，实例化空表单
-    return render(request,'Upload/register.html',{'uf':uf})  #只有刚显示时才起作用
-
-'''
-前置条件：path是文件的绝对路径，path下的文件要保证存在并且是docx格式
-后置条件：将path下的docx文件读取出来，并保存在‘G:/’下的txt文档中；文档如果不存在，服务器会停止响应，后期对这个异常进行
-处理；后面预计会加上对doc文件的支持，可能是将doc文件转换为docx文件处理
-'''
-def read_doc(request,path):  #阅读docx文档,path为文件的路径
-        store_path = 'G:/'  #txt文件保存的路径
-        doc = docx.Document(path)  #根据路径打开一个docx对象
-        file_name = path[path.rfind('/') + 1:path.find('.')]  #提取路径中的文件名,不包含后缀
-        outfile = open(store_path + file_name + '_temp.txt','w')   #创建一个临时的txt文件
-        for i in range(len(doc.paragraphs)):  #将docx文档里的数据写入txt文档中
-            outfile.write(doc.paragraphs[i].text + '\n')
-        outfile.close()  #结束写入
->>>>>>> 9f36014a873b3d7ceed0413346a988b74c49858f
