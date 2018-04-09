a2bc65e6c7a198a6babc47affff566f182cebc14
447428054 <447428054@qq.com>
2018-04-09 14:30:33 +0800

    json打包笔记数据

--- Note/views.py
+++ Note/views.py
@@ -4,39 +4,46 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
+import json
 
 
 # Create your views here.
 
 def AllNotes(request):
     notes = models.Note.objects.all()
-    return render(request, 'Note/Note.html', {'notes': notes})
+    Notes = json.dumps(notes , ensure_ascii=False)
+    return HttpResponse(Notes)
 
 
 def Note_Page(request, note_id):
     note = models.Note.objects.get(pk=note_id)
-    return render(request, 'Note/Note_Page.html', {'note': note})
+    Note= json.dumps(note, ensure_ascii=False)
+    return HttpResponse(Note)
 
 
 def Edit_Page(request, note_id):
     if str(note_id) == '0':
         return render(request, 'Note/Edit_Page.html')
     note = models.Note.objects.get(pk=note_id)
-    return render(request, 'Note/Edit_Page.html', {'note': note})
+    Note=json.dumps(note, ensure_ascii=False)
+    return HttpResponse(Note)
 
 
 def Edit_action(request):
-    title = request.POST.get('title', 'TITLE')
-    content = request.POST.get('content', 'CONTENT')
-    note_id= request.POST.get('note_id', '0')
+    if request.method =='Get':
+        title = request.POST.get('title', 'TITLE')
+        content = request.POST.get('content', 'CONTENT')
+        note_id= request.POST.get('note_id', '0')
 
-    if note_id == '0':
-        models.Note.objects.create(title=title, content=content)
-        notes = models.Note.objects.all()
-        return render(request, 'Note/Note.html', {'notes': notes})
+        if note_id == '0':
+            models.Note.objects.create(title=title, content=content)
+            notes = models.Note.objects.all()
+            Notes=json.dumps(notes, ensure_ascii=False)
+            return HttpResponse(Notes)
 
-    note=models.Note.objects.get(pk=note_id)
-    note.title = title
-    note.content = content
-    note.save()
-    return render(request, 'Note/Note_Page.html', {'note': note})
+        note=models.Note.objects.get(pk=note_id)
+        note.title = title
+        note.content = content
+        note.save()
+        Note=json.dumps(note, ensure_ascii=False)
+        return HttpResponse(Note)
